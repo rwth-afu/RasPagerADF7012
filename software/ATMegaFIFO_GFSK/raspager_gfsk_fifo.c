7dce4ead4d764795e2cceb2b3a733809531c1187
Ralf Wilke <wilke@ihf.rwth-aachen.de>
2017-02-02 11:39:16 +0100

    Untested code for GFSK

--- software/ATMegaFIFO_GFSK/raspager_gfsk_fifo.c
+++ software/ATMegaFIFO_GFSK/raspager_gfsk_fifo.c
@@ -1,19 +1,35 @@
+/*
+ * Copyright (C) 2017 Amateurfunkgruppe der RWTH Aachen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 
-
-
-// ############################################################################
-// #                                 Config                                   #
-// ############################################################################
-
-
-
-// Frequency
+// AT Mega Frequency
 #define F_CPU 8000000
 
-// TRM_DATA -> This pin shifts out the data to the transmitter PB1
+// TRM_DATA -> This pin shifts out the data to the transmitter
+// ATMega: PB1, ADF7012: TxData
 #define TRM_DATA_PORT      PORTB
 #define TRM_DATA_DDR       DDRB
 #define TRM_DATA_BIT       1
+
+// TRM_CLOCK -> This pin is the data clock from the ADF7012 to the ATMega
+// ATMega: PD2 (INT0), ADF7012: TXCLK
+#define TRM_CLOCK_PORT      PORTD
+#define TRM_CLOCK_DDR       DDRD
+#define TRM_CLOCK_BIT       2
 
 // RPI_HANDSHAKE -> This pin tells the RPI, when it is able to receive data PD3
 #define RPI_HANDSHAKE_PORT PORTD
@@ -35,6 +51,7 @@
 #define RPI_CLOCK_PIN      PIND
 #define RPI_CLOCK_BIT      1
 
+// For GFSK: 
 
 
 // ############################################################################
@@ -103,83 +120,56 @@ if(FIFOBUF_##name##_pop >= sizeof(FIFOBUF_##name##_data)) FIFOBUF_##name##_pop =
 
 FIFO_Init(buffer, uint8_t, 512);
 
-
-
-// ############################################################################
-// #                           1200 Hz Interrupt                              #
-// ############################################################################
-
-
-
-ISR(TIMER2_COMP_vect)
-// ============================================================================
-{ // TIMER2_COMP_vect()
+// INT 1 Service Routine. Executed on falling edge. Set new bit on each execution.
+ISR(INT1_vect) {
 
 	// Handle shifting out of data
-	if(!FIFO_Empty(buffer))      // Data in buffer?
-	{
+	if (!FIFO_Empty(buffer)) {     // Data in buffer?
 		uint8_t databit;
 		FIFO_Pop(buffer, databit); // Get bit from FIFO
-		if(databit)                // Databit is "1"?
-		{
+		if (databit) {                // Databit is "1"?
 			SET_OUTPUT(TRM_DATA);    // Set data output high
-		}
-		else                       // Databit is "0"?
-		{
+		} else {                       // Databit is "0"?
 			CLR_OUTPUT(TRM_DATA);    // Set data output low
 		}
 		SET_OUTPUT(RPI_SHIFTING);  // Tell RPI that currently data is shifted out to the transmitter
-	}
-	else                         // No data in buffer anymore?
-	{
+	} else {                         // No data in buffer anymore?
 		CLR_OUTPUT(TRM_DATA);      // Set data output low
 		CLR_OUTPUT(RPI_SHIFTING);  // Tell RPI that no data is shifted out at the moment
 	}
+}
 
-} // TIMER2_COMP_vect()
-// ============================================================================
-
-
-
-// ############################################################################
-// #                               Main-Loop                                  #
-// ############################################################################
-
-
-
-int main()
-// ============================================================================
-{ // main()
-
-	// Initialize timer for 1200 Hz
-	TCCR2 = (0<<FOC2)  // No force of output compare match
-	| (1<<WGM21)
-	| (0<<WGM20) // CTC
-	| (0<<COM21)
-	| (0<<COM20) // OCx disconnected
-	| (1<<CS22)
-	| (0<<CS21)
-	| (0<<CS20); // Clock /  256 (from prescaler)
-	OCR2  = F_CPU / 64 / 1207;
-	TIMSK = (1<<OCIE2);
-
+void init() {
 	// Enable outputs
 	INIT_OUTPUT(TRM_DATA);
 	INIT_OUTPUT(RPI_HANDSHAKE);
 	INIT_OUTPUT(RPI_SHIFTING);
 
+	// Set up INT1_vect
+	
+	// Set PD2 = INT0 as Input
+	DDRD &= ~(1 << DDD2);
+    
+	// Enable Falling edge activation
+	MCUCR = (MCUCR & 0b11111100) | (1 << ISC01);
+	
+	// Enable INT0 interrupt
+	GICR |= (1 << INT0); 
+	
 	// Enable interrupts
 	sei();
 
+}
+
+int main() {
+	init();
+	
 	while(1)
 	{
 		// Handle receiving of data
-		if(FIFO_Full(buffer))                      // Buffer full?
-		{
+		if (FIFO_Full(buffer)) {                      // Buffer full?
 			CLR_OUTPUT(RPI_HANDSHAKE);               // Set handshake output to low -> Not able to receive anymore
-		}
-		else                                       // Buffer not full yet?
-		{
+		} else {                                       // Buffer not full yet?
 			static uint8_t last_clk_state;
 			uint8_t current_clk_state;
 			uint8_t databit;
@@ -187,10 +177,8 @@ int main()
 			current_clk_state = IS_INPUT(RPI_CLOCK); // Get current clock state
 			databit           = IS_INPUT(RPI_DATA);  // Get data bit value
 
-			if(    (last_clk_state    == 1)          // Clock edge from high to low?
-			&& (current_clk_state == 0)
-			)
-			{
+			// Clock edge from high to low?
+			if ((last_clk_state == 1) && (current_clk_state == 0)) {
 				cli();                                 // Disable interrupt
 				FIFO_Push(buffer, databit);            // Push data to buffer
 				sei();                                 // Enable interrupt
@@ -201,8 +189,6 @@ int main()
 			SET_OUTPUT(RPI_HANDSHAKE);               // Set handshake output to high -> Able to receive data
 		}
 	}
-
-} // main()
-// ============================================================================
-
+	return 0;
+} 
 
